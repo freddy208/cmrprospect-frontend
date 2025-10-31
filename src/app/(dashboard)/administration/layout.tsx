7062ae390231e7f4f6449ded3d304954400ed0e2
freddy208 <kfreddypatient@gmail.com>
2025-10-31 19:10:12 +0100

    Admin v3

--- src/app/(dashboard)/administration/layout.tsx
+++ src/app/(dashboard)/administration/layout.tsx
@@ -1,7 +1,6 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { AuthProvider } from "@/hooks/useAuth";
 import { AdminProvider } from "@/hooks/useAdminContext";
 
